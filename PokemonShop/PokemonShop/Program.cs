c6036457c2dbebcdb3ececcae7fe8d74c8e731f2
Aliaksandr-Nad <58305547+aliaksandr-nad@users.noreply.github.com>
2021-05-01 15:42:45 +0300

    pokemon shop - initial commit

---
+++ PokemonShop/PokemonShop/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace PokemonShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration((context, builder) =>
+                    {
+                        builder
+                            .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                            .AddYamlFile("appsettings.yaml", false, true)
+                            .AddYamlFile($"appsettings.{context.HostingEnvironment.EnvironmentName.ToLower()}.yaml",
+                                true, true)
+                            .AddEnvironmentVariables();
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ PokemonShop/PokemonShop/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace PokemonShop
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        
+        private string Version => Configuration.GetValue<string>("Service:Version");
+
+        private string Title => Configuration.GetValue<string>("Service:Title");
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = Title, Version = Version}); });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            
+            app.UseHttpsRedirection()
+                .UseRouting()
+                .UseSwagger()
+                .UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PokemonShop v1"));
+
+            app.UseStaticFiles();
+
+            app.UseAuthorization()
+                .UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
